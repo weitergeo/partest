1d57a9bd5dcc610eeddc8cefe4663986e3fd09da
weitergeo <weitergeo@gmail.com>
2018-10-23 20:39:30 +0300

    Add files via upload

---
+++ TestVal_start.bat
@@ -0,0 +1,2 @@
+
+python testpar.py
---
+++ libs_install.bat
@@ -0,0 +1,9 @@
+set python_ver=27
+cd \
+cd \python%python_ver%\Scripts\
+pip install pygsheets
+pip install --upgrade oauth2client
+pip install xlsxwriter
+pip install csv
+pip install os
+pip install sys
