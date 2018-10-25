d78a0d23913f41d104ec1eca839b003a79b7764e
weitergeo <weitergeo@gmail.com>
2018-10-25 19:14:18 +0300

    Add files via upload

--- libs_install.bat
+++ libs_install.bat
@@ -1,9 +1,8 @@
-set python_ver=27
-cd \
-cd \python%python_ver%\Scripts\
+pushd %GLLEGACY_PYTHON_HOME%\Scripts\
 pip install pygsheets
 pip install --upgrade oauth2client
-pip install xlsxwriter
 pip install csv
 pip install os
 pip install sys
+pip install pyinstaller
+popd
--- testpar.py
+++ testpar.py
@@ -1,8 +1,6 @@
 import pygsheets
 import csv
-import os
 import sys
-import time
 
 #Query function
 def query_yes_no(question, default="yes"):
@@ -40,7 +38,7 @@ def query_yes_no(question, default="yes"):
 #print(os.getcwd())
 
 #Pasre list from LOCAL .CSV to list
-CsvList = list(csv.reader(open('test_2.csv')))
+CsvList = list(csv.reader(open('../../../build/win/BotReports/test/statistics.csv')))
 
 #Parse level number and hash from .CSV
 levelHashCsv = CsvList[0][0]
@@ -59,8 +57,7 @@ gc = pygsheets.authorize(service_file="ProjectTest-82e6b523785c.json")
 sh = gc.open('Level Tester')
 
 #Select the first sheet
-wks = sh.worksheet('index', 1)
-wks2 = sh.worksheet('index', 2)
+wks = sh.worksheet_by_title('LevelData')
 #Parse list from Google Sheets table
 GSheetList = wks.get_col(levelNoCsv + 1, returnas='matrix')
 
@@ -73,28 +70,27 @@ del GSheetList[0:2]
 IntSheet = [int(j) for j in GSheetList]
 
 #Creating result list the same size as the other lists
-ResultList = IntCsv
 
 #Summing numbers in list
-for i in range(len(IntSheet)):
-    ResultList[i] = IntSheet[i] + IntCsv[i]
     
 #Hashes comparison
 if levelHashGS == '0':
-    wks.update_col(levelNoCsv + 1, values=ResultList, row_offset=2)
-    wks.update_value((1,levelNoCsv + 1),val=levelHashCsv)
+    wks.update_col(levelNoCsv + 1, values=IntCsv, row_offset=2)
+    wks.update_cell((1,levelNoCsv + 1),val=levelHashCsv)
+    print "Data successfully loaded to Google Sheets!"
 elif levelHashGS == levelHashCsv:
-    wks.update_col(levelNoCsv + 1, values=ResultList, row_offset=2)
+    for i in range(len(IntCsv)):
+        IntCsv[i] = IntSheet[i] + IntCsv[i]
+    wks.update_col(levelNoCsv + 1, values=IntCsv, row_offset=2)
+    print "Data successfully loaded to Google Sheets!"
 else:
     if query_yes_no("Level was changed rewrite data?", default="no") == True:
-        wks.update_col(levelNoCsv + 1, values=ResultList, row_offset=2)
+        wks.update_col(levelNoCsv + 1, values=IntCsv, row_offset=2)
+        wks.update_cell((1,levelNoCsv + 1),val=levelHashCsv)
         print "Data was rewritten!"
     else:
         print "All data is lost"
         # @TODO search for exists hashes and write data to existing hashes, or write it to new columns
-        #wks2.update_col(levelNoCsv + 1, values=ResultList, row_offset=2
-        # dfjlkdgsnglsnlgnsdl)
+        #wks2.update_col(levelNoCsv + 1, values=IntCsv, row_offset=2
     print levelHashGS
     print levelHashCsv
-print "Data successfully loaded to Google Sheets!"
-time.sleep(5)
